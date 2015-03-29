c204f10ba9cf3b05a222c5e54d8e2d05401ab1bd
Sal Espinosa <sespinos@gmail.com>
2015-03-29 12:52:16 -0400

    Use anonymous functions to DRY the code

--- ga/week5/js/project.js
+++ ga/week5/js/project.js
@@ -1,59 +1,21 @@
-document.getElementById("a10").onmouseover = add10;
-document.getElementById("a20").onmouseenter = add20;
-document.getElementById("a30").onmouseenter = add30;
+document.getElementById("a10").onmouseover = function(){addSubtract(10)};
+document.getElementById("a20").onmouseenter = function(){addSubtract(20)};
+document.getElementById("a30").onmouseenter = function(){addSubtract(30)};
 
-document.getElementById("red").onmouseenter = changeRed;
-document.getElementById("out").onmouseenter = changeBlank;
-document.getElementById("blue").onmouseenter = changeBlue;
+document.getElementById("red").onmouseenter = function(){changeColor("red")};
+document.getElementById("out").onmouseenter = function(){changeColor("white")};
+document.getElementById("blue").onmouseenter = function(){changeColor("blue")};
 
-document.getElementById("n10").onmouseenter = subtract10;
-document.getElementById("n20").onmouseenter = subtract20;
-document.getElementById("n30").onmouseenter = subtract30;
+document.getElementById("n10").onmouseenter = function(){addSubtract(-10)};
+document.getElementById("n20").onmouseenter = function(){addSubtract(-20)};
+document.getElementById("n30").onmouseenter = function(){addSubtract(-30)};
 
-function add10() {
+function addSubtract(numToAdd) {
 	currentVal = parseInt(document.getElementById("out").innerHTML);
-	newVal = currentVal + 10;
+	newVal = currentVal + numToAdd;
 	document.getElementById("out").innerHTML = newVal;
 }
 
-function add20() {
-	currentVal = parseInt(document.getElementById("out").innerHTML);
-	newVal = currentVal + 20;
-	document.getElementById("out").innerHTML = newVal;
-}
-
-function add30() {
-	currentVal = parseInt(document.getElementById("out").innerHTML);
-	newVal = currentVal + 30;
-	document.getElementById("out").innerHTML = newVal;
-}
-
-function subtract10() {
-	currentVal = parseInt(document.getElementById("out").innerHTML);
-	newVal = currentVal - 10;
-	document.getElementById("out").innerHTML = newVal;	
-}
-
-function subtract20() {
-	currentVal = parseInt(document.getElementById("out").innerHTML);
-	newVal = currentVal - 20;
-	document.getElementById("out").innerHTML = newVal;	
-}
-
-function subtract30() {
-	currentVal = parseInt(document.getElementById("out").innerHTML);
-	newVal = currentVal - 30;
-	document.getElementById("out").innerHTML = newVal;	
-}
-
-function changeRed() {
-	document.getElementById("out").style.backgroundColor = "red";
-}
-
-function changeBlank() {
-	document.getElementById("out").style.backgroundColor = "white";
-}
-
-function changeBlue() {
-	document.getElementById("out").style.backgroundColor = "blue";
+function changeColor(newColor) {
+	document.getElementById("out").style.backgroundColor = newColor;
 }
