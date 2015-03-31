6d70f289ca7592ef0230e02730e4a8b8fb57c83c
Sal Espinosa <sespinos@gmail.com>
2015-03-30 23:33:23 -0400

    Update final project to include some basic audio

--- ga/final/js/main.js
+++ ga/final/js/main.js
@@ -1,73 +1,144 @@
+//**INITIAL SETUP**//
+
 // setup
 var onOff = "off";
-var frequency = 600/4;
+var toClearOnOff = 0;
 var padsArray = document.getElementsByClassName("pads");
-console.log(padsArray.length);
 
-distributeFrequency(frequency);
+// pad colors
+var padBackOff = "A4A6A5";
+var padBackOffBlink = "CFD2D1";
+var padBackOn = "67A7CE";
+var padBackOnBlink = "#7FC8F6";
+
+// instrument pattern arrays
+var bassPattern = [];
+var snarePattern = [];
+var highPattern = [];
+var highOpenPattern = [];
+
+// initial beat per minute values
+var bpm = 100; //100 beats per minute hard coded for now
+var mspbeat = (60*1000)/100; //600 milliseconds for each beat
+var msp16th = mspbeat/4; //150 milliseconds for each sixteenth note => each pad for 32 pads assuming 2 measures.
+// var frequency = 600/(padsArray.length/2);
+
+// add attributes and onclick behavior to pads
+setPads();
 
 
-// check setup
-var printFrequency = document.getElementById("pad3").getAttribute("freqBlinkOff");
-console.log(printFrequency);
 
 
-// On and Off buttons
-var toClear = 0;
-
+// Set On button functions
 document.getElementById("start").onclick = function() {
 	if(onOff == "on") {
 		return;
 	} else {
 		onOff = "on";
-		console.log(onOff);
 		blinkOn();
-		toClear = setInterval(function(){blinkOn();}, frequency*16);
-	}
-}
-
-document.getElementById("stop").onclick = function() {
-	if(onOff == "off") {
-		return;
-	} else {
-		onOff = "off";
-		clearInterval(toClear);
+		toClearOnOff = setInterval(function(){blinkOn();}, msp16th*padsArray.length);
 	}
 }
 
 
-
+// blinking function for pads
 function blinkOn() {
 	if (onOff == "off") {
 		return;
 	} else {
 		for (i=0; i<padsArray.length; i++) {
 			howToBlink(padsArray[i],i);
+			howToPlay(i);
 		}
-
-		// setTimeout(function() {document.getElementById("pad1").style.backgroundColor = "#FFFFFF";}, frequency*0);
-		// setTimeout(function() {document.getElementById("pad1").style.backgroundColor = "#AAAAAA";}, frequency*1);
-		// setTimeout(function() {document.getElementById("pad2").style.backgroundColor = "#FFFFFF";}, frequency*4);
-		// setTimeout(function() {document.getElementById("pad2").style.backgroundColor = "#AAAAAA"}, frequency*5);
-		// setTimeout(function() {document.getElementById("pad3").style.backgroundColor = "#FFFFFF";}, frequency*8);
-		// setTimeout(function() {document.getElementById("pad3").style.backgroundColor = "#AAAAAA"}, frequency*9);
-		// setTimeout(function() {document.getElementById("pad4").style.backgroundColor = "#FFFFFF";}, frequency*12);
-		// setTimeout(function() {document.getElementById("pad4").style.backgroundColor = "#AAAAAA"}, frequency*13);
 	}
 }
-
-
-// set pad attributes for how frequently to blink
-function distributeFrequency (freq) {
-	for (i=0; i < padsArray.length; i++) {
-		padsArray[i].setAttribute("freqBlinkOn", i*4);
-		padsArray[i].setAttribute("freqBlinkOff", (i*4)+1);
-	}
-}
-
 
 // set delays for blinking behavior on each pad (repeating from the beginning is handled by setInterval in original call from onclick)
 function howToBlink(padElement, count) {
-		setTimeout(function() {padElement.style.backgroundColor = "#FFFFFF";}, frequency*(count*4));
-		setTimeout(function() {padElement.style.backgroundColor = "#AAAAAA";}, frequency*((count*4)+1));
+	if (bassPattern[padElement.getAttribute("padNumber")] === 0) {
+		setTimeout(function() {padElement.style.backgroundColor = padBackOffBlink;}, msp16th*(count));
+		setTimeout(function() {padElement.style.backgroundColor = padBackOff;}, msp16th*(count+1));
+	} else {
+		setTimeout(function() {padElement.style.backgroundColor = padBackOnBlink;}, msp16th*(count));
+		setTimeout(function() {padElement.style.backgroundColor = padBackOn;}, msp16th*(count+1));		
+	}
 }
+
+
+// Set On button functions
+document.getElementById("stop").onclick = function() {
+	if(onOff == "off") {
+		return;
+	} else {
+		onOff = "off";
+		clearInterval(toClearOnOff);
+	}
+}
+
+
+
+
+function howToPlay(count) {
+	if (bassPattern[count] == 0) {
+		setTimeout(function() {document.getElementById("silence").play();}, msp16th*count);
+	} else {
+		setTimeout(function() {document.getElementById("kick").play();}, msp16th*count);
+		console.log(bassPattern);
+	}
+}
+
+
+
+
+
+// set pads with onclick behavior
+// function setPads() {
+// 	for (i=0; i < padsArray.length; i++) {
+// 		padsArray[i].setAttribute("padOn") = "off";
+// 		padsArray[i].onclick = togglePad();
+// 	}
+// }
+
+function setPads() {
+	for (i=0; i < padsArray.length; i++) {
+		padsArray[i].setAttribute("padNumber", i);
+		bassPattern.push(0);
+		padsArray[i].onclick = function() {
+			var padNumber = this.getAttribute("padNumber");
+			if (bassPattern[padNumber] == 0) {
+				bassPattern[padNumber] = 1;
+				this.style.backgroundColor = padBackOn;
+			} else {
+				bassPattern[padNumber] = 0;
+				this.style.backgroundColor = padBackOff;		
+			}
+		}	
+	}
+}
+
+
+
+
+
+// testing stuff
+
+// console.log(padsArray.length);
+
+// distributeFrequency(msp16th);
+// // set pad attributes for how frequently to blink
+// function distributeFrequency (freq) {
+// 	for (i=0; i < padsArray.length; i++) {
+// 		padsArray[i].setAttribute("freqBlinkOn", i*4);
+// 		padsArray[i].setAttribute("freqBlinkOff", (i*4)+1);
+// 	}
+// }
+
+
+
+
+
+// // check setup
+// var printFrequency = document.getElementById("pad3").getAttribute("freqBlinkOff");
+// console.log(printFrequency);
+
+
