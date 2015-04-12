e934ba282a4233262bb5806152beea0a1114f5ef
Sal Espinosa <sespinos@gmail.com>
2015-04-12 13:29:49 -0400

    Make timing for generate very long

--- ga/week6/pick-your-external/js/main.js
+++ ga/week6/pick-your-external/js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 		} else {
 			closeIpsum();
 			changeIpsum(clicked);
-			setTimeout(function(){generateIpsum();}, 600);
+			setTimeout(function(){generateIpsum();}, 5000);
 		}
 	
 	});
