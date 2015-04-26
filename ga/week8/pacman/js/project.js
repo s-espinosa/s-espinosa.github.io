8452ef18886c7b517e5390438cab47b4496909df
Sal Espinosa <sespinos@gmail.com>
2015-04-26 13:47:43 -0400

    Initial commit for pacman assignment

---
+++ ga/week8/pacman/js/project.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+
+	setUp();
+
+	//Your code goes here
+
+	var numClicks = 0;
+	var ghostNumber = "";
+	var decimalMove = 0;
+	var percentMove = "";
+	var bodyWidth = $("body").width();
+
+	$("body").click(function(){
+
+		if (numClicks < 4) {
+			numClicks++;
+			ghostNumber = "#ghost" + numClicks;
+			decimalMove = (.7 - (.1*numClicks))*bodyWidth;
+
+			console.log($("body").width());
+
+			$(ghostNumber).animate({
+				left: decimalMove
+			}, 2000);
+
+		} else {
+			numClicks = 0
+
+			$("#pacman").animate({
+				left: "45%"
+			}, 2000).animate({
+				left: "110%"
+			}, 2000, $("#pacman").css("left", "-100px"));
+
+			$("#ghost1, #ghost2, #ghost3, #ghost4").animate({
+				left: "110%"
+			}, 2000);
+
+
+		}
+
+
+
+		// $("#ghost1").animate({
+		// 	left: "70%"
+		// }, 3000);
+
+
+	});
+
+
+	$("body").mouseenter(function(){
+		$("h1").fadeOut(500);
+	});
+});
+
+function setUp() {
+	$("body, html, #wrapper").height($(window).height());
+	$("body, html, #wrapper").width($(window).width());
+}
+
+$(window).resize(function() {
+	setUp();
+});
