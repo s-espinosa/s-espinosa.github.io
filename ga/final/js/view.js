2433e9412801b43fea1eb38c6c837ee8f7c08bfa
Sal Espinosa <sespinos@gmail.com>
2015-05-04 18:24:20 -0400

    Refactor JSON pull with callbacks

--- ga/final/js/view.js
+++ ga/final/js/view.js
@@ -1,4 +1,65 @@
 $(document).ready(function(){
+	//Pull data for page has callback to pull JSON template
+	//pull template has callback to create page
+
+	//pullPageData("detail", "webdetail", 0);
+
+
+	function pullSingletPageData(pageType, pageLocation, pageToPull) {
+		//pageType is either singlet or grid
+		//pageToPull will be pulled from the button id. Should be able to use the index of the links/divs/images in the main area
+
+
+		var JSONLocation = "data/" + pageLocation + ".json";
+
+		$.getJSON(JSONLocation, function(data) {
+			console.log(data);
+			var pageData = data[pageToPull];
+			pullSingletTemplate(pageData);
+		});		
+	} 
+
+
+	function pullSingletTemplate(pageData){
+		$.getJSON("data/detailtemplate.json", function(data) {
+			console.log(data);
+			createSingletPage(pageData, data);
+		});	
+	}
+
+	function createSingletPage(pageData, templateData) {
+		var HTMLtoInsert = templateData[0] + pageData.image + SingletTemplate[1] + pageDAta.link + SingletTemplate[2] + pageData.title + SingletTemplate[3] + pageData.assignment + SingletTemplate[4];
+		$("#mainWrapper").html(HTMLtoInsert);		
+	}
+
+	pullSingletPageData("singlet", "webdetail", 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 	//ASSIGN CLICKS TO NAV BUTTONS//
@@ -16,44 +77,41 @@ $(document).ready(function(){
 
 
 
-	//ACTUAL FUNCTIONS FOR CLICKS//
-	var SingletTemplate = pullJSON("detailtemplate");
+	// //ACTUAL FUNCTIONS FOR CLICKS//
+	// var SingletTemplate = pullJSON("detailtemplate");
 
-	function makeGridPage(pageToMake) {
-		var JSONGridArray = pullJSON(pageToMake);
+	// function makeGridPage(pageToMake) {
+	// 	var JSONGridArray = pullJSON(pageToMake);
 
-		//function to make a layout with multiple items
-		//run after pickPosts so that activePosts is current
-	}
-
-
-	function makeSingletPage(pageType, pageToMake) {
-		var JSONLocation = pageType + "detail"
-		var JSONSingletArray = pullJSON(JSONLocation);
-		var imageSource = "img/detail/" + pageType + "/" + pageType + pageToMake + "detail.gif";
-		var detailIndex = (pageToMake * 1) - 1;
+	// 	//function to make a layout with multiple items
+	// 	//run after pickPosts so that activePosts is current
+	// }
 
 
-		//all of this below just seems to be happening too fast. Try ot make it a callback to the JSON pull itself.//
-		//means that you'll need to push all the JSON at once (template and detail)//
-		var HTMLtoInsert = SingletTemplate[0] + imageSource + SingletTemplate[1] + JSONSingletArray[detailIndex].link + SingletTemplate[2] + JSONSingletArray[detailIndex].title + SingletTemplate[3] + JSONSingletArray[detailIndex].assignment + SingletTemplate[4];
-		$("#mainWrapper").html(HTMLtoInsert);
-		//function to make a layout with a single focus
-		//run after pickPosts so that activePosts is current
-	}
+	// function makeSingletPage(pageType, pageToMake) {
+	// 	var JSONLocation = pageType + "detail"
+	// 	var JSONSingletArray = pullJSON(JSONLocation);
+	// 	var imageSource = "img/detail/" + pageType + "/" + pageType + pageToMake + "detail.gif";
+	// 	var detailIndex = (pageToMake * 1) - 1;
 
 
-	function pullJSON(JSONPage) {
-		var JSONLocation = "data/" + JSONPage + ".json";
+	// 	//all of this below just seems to be happening too fast. Try ot make it a callback to the JSON pull itself.//
+	// 	//means that you'll need to push all the JSON at once (template and detail)//
+	// 	var HTMLtoInsert = SingletTemplate[0] + imageSource + SingletTemplate[1] + JSONSingletArray[detailIndex].link + SingletTemplate[2] + JSONSingletArray[detailIndex].title + SingletTemplate[3] + JSONSingletArray[detailIndex].assignment + SingletTemplate[4];
+	// 	$("#mainWrapper").html(HTMLtoInsert);
+	// 	//function to make a layout with a single focus
+	// 	//run after pickPosts so that activePosts is current
+	// }
 
-		$.getJSON(JSONLocation, function(data) {
-			console.log(data);
-			return data;
-		});
-	}
 
+	// function pullJSON(JSONPage) {
+	// 	var JSONLocation = "data/" + JSONPage + ".json";
 
-	makeSingletPage("web", "01");
+	// 	$.getJSON(JSONLocation, function(data) {
+	// 		console.log(data);
+	// 		return data;
+	// 	});
+	// }
 
 
 
