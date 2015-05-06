6514a5cdb05ad9ab431a17a8d7728c9457d03c23
Sal Espinosa <sespinos@gmail.com>
2015-05-05 23:03:42 -0400

    Add fade out fade for navigation

--- ga/final/js/view.js
+++ ga/final/js/view.js
@@ -2,6 +2,24 @@ $(document).ready(function(){
 
 
 
+	//ASSIGN CLICKS TO NAV BUTTONS//
+	$("#webNav").click(function(){
+		$("#mainWrapper").fadeOut("slow", function() {
+			pullGridPageData("webgrid")
+		});
+	});
+
+	$("#audioNav").click(function(){
+		$("#mainWrapper").fadeOut("slow", function() {
+			pullGridPageData("audiogrid")
+		});
+	});
+
+	$("#videoNav").click(function(){
+		makeSingletPage("video");
+	});	
+
+
 
 
 	//CREATE SINGLET PAGES: Pages showing the detail of a single project.//
@@ -30,6 +48,9 @@ $(document).ready(function(){
 
 	//SINGLET TEST//
 	// pullSingletPageData("audiodetail", 3);
+
+
+
 
 
 
@@ -76,106 +97,6 @@ $(document).ready(function(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	//ASSIGN CLICKS TO NAV BUTTONS//
-	$("#webNav").click(function(){
-		makeGridPage("web");
-	});
-
-	$("#audioNav").click(function(){
-		makeGridPage("audio");
-	});
-
-	$("#videoNav").click(function(){
-		makeSingletPage("video");
-	});	
-
-
-
-	// //ACTUAL FUNCTIONS FOR CLICKS//
-	// var SingletTemplate = pullJSON("detailtemplate");
-
-	// function makeGridPage(pageToMake) {
-	// 	var JSONGridArray = pullJSON(pageToMake);
-
-	// 	//function to make a layout with multiple items
-	// 	//run after pickPosts so that activePosts is current
-	// }
-
-
-	// function makeSingletPage(pageType, pageToMake) {
-	// 	var JSONLocation = pageType + "detail"
-	// 	var JSONSingletArray = pullJSON(JSONLocation);
-	// 	var imageSource = "img/detail/" + pageType + "/" + pageType + pageToMake + "detail.gif";
-	// 	var detailIndex = (pageToMake * 1) - 1;
-
-
-	// 	//all of this below just seems to be happening too fast. Try ot make it a callback to the JSON pull itself.//
-	// 	//means that you'll need to push all the JSON at once (template and detail)//
-	// 	var HTMLtoInsert = SingletTemplate[0] + imageSource + SingletTemplate[1] + JSONSingletArray[detailIndex].link + SingletTemplate[2] + JSONSingletArray[detailIndex].title + SingletTemplate[3] + JSONSingletArray[detailIndex].assignment + SingletTemplate[4];
-	// 	$("#mainWrapper").html(HTMLtoInsert);
-	// 	//function to make a layout with a single focus
-	// 	//run after pickPosts so that activePosts is current
-	// }
-
-
-	// function pullJSON(JSONPage) {
-	// 	var JSONLocation = "data/" + JSONPage + ".json";
-
-	// 	$.getJSON(JSONLocation, function(data) {
-	// 		console.log(data);
-	// 		return data;
-	// 	});
-	// }
-
-
-
-	//FUNCTIONS TO OPEN AND CLOSE WINDOWS (RENAME)//
-	function changeArticle(clicked){
-		var clickedClass = "." + clicked;
-		// function closeArticle(){
-		// 	$("main").fadeOut("slow");
-		// 	// $(".ipsum").slideUp("slow");
-		// }
-		
-
-
-		// $("#activeLorem").removeAttr("id");
-		// $(clickedClass).attr("id", "activeLorem");
-
-
-	}
-
-
-	function generateArticle(){
-	// 	var numParagraphs = $("#paragraphs").val();
-
-	// 	if (numParagraphs === "") {
-	// 		return;
-	// 	} else {
-	// 		$("div p").slice(0, numParagraphs).fadeIn("slow");
-	// 		$(".ipsum").slideDown("slow");
-	// 	}
-	}
-
-
-	//INITIAL PAGE SETUP USING FUNCTIONS//
-	// generateArticle(currentArticle);
 
 
 
