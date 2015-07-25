d483eef78714acfab6eb24b5534bd04171b15f26
Sal Espinosa <sespinos@gmail.com>
2015-07-25 11:11:19 -0400

    Add pads and personal site, restart Hartl

---
+++ pads/js/padsjs/pads.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	//LAYOUT//
+	//Push the pads to the site from the template.
+
+	//first need to get the template from the JSON data
+
+	//add the controls
+
+	//add the pads
+
+	//add the sound pads
+
+
+
+	//PLAYING//
+	//this should be the code that actually causes the pads to play.
+
+	//parent function has to include the code to play the sound, then the next line animates the pad
+	//animate one pad blinking using... jquery and css {backgroundcolor: blinkColor}, fast?
+	//then use a callback to fade it out
+	//then use a callback to animate the next pad
+
+
+
+})
