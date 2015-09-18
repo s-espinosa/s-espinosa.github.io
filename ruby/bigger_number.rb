db415b52d13b0e9d300c2f51c0aea197679e91e7
Sal Espinosa <sespinos@gmail.com>
2015-09-17 21:37:25 -0400

    Start the Chris Pine Ruby book.

---
+++ ruby/bigger_number.rb
@@ -0,0 +1,4 @@
+puts "What is your favorite number?"
+fav_number = gets.chomp.to_i
+better_number = fav_number +1
+puts "Hmmm... " + fav_number.to_s + " is an o.k. favorite number, but " + better_number.to_s + " is a better number."
