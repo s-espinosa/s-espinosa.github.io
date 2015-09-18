c2bede307f9f398c987e1497791a27ca3f266387
Sal Espinosa <sespinos@gmail.com>
2015-09-18 10:03:03 -0400

    Add Ruby practice from work computer

---
+++ ruby/calc.rb
@@ -0,0 +1,8 @@
+puts "Hours in a year: "
+puts 24*365
+puts "Minutes in a decade: "
+puts 24*60*60*365*10
+puts "Age in seconds: "
+puts 35*365*24*60*60
+puts "Author's age: "
+puts 1160*1000000/60/60/24/365
