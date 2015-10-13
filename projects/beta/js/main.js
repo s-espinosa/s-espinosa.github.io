36b61f0d08b3205dee651caf2c6626e88862ab60
Sal Espinosa <sespinos@gmail.com>
2015-10-13 18:04:12 -0400

    Begin update of JavaScript to place random rectangles.

--- projects/beta/js/main.js
+++ projects/beta/js/main.js
@@ -1,25 +1,29 @@
 $(document).ready(function(){
 
   // Box constructor function
-    function box(max_height, max_width, distance_from_top, percent_movement, speed) {
+    function Box(passed_max_height, passed_max_width, passed_distance_from_top) {
+        
+        this.max_height = passed_max_height;
+        this.max_width = passed_max_width;
+        this.distance_from_top = passed_distance_from_top;
+
+        this.actual_height = Math.round(Math.random() * this.max_height);
+        this.actual_width = Math.round(Math.random() * this.max_width);
+
         
 
 
+        // this.animate() = function(callback){
 
-        this.animate() = function(callback){
-
-        }
+        // }
     };
 
 
-var box1 = new box(280, 700, 90, 70%, 400);
-var box2 = new box(280, 700, 90, 70%, 400);
-var box3 = new box(280, 700, 90, 70%, 400);
-var box4 = new box(280, 700, 90, 70%, 400);
+var box1 = new box(280, 700, 90);
+var box2 = new box(280, 700, 90);
+var box3 = new box(280, 700, 90);
+var box4 = new box(280, 700, 90);
 
 var boxes = [box1, box2, box3, box4];
-
-// each
-
 }
 
