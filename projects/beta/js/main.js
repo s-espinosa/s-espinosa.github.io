3bcd5a46fedd468ef5649710743a84f0b109bfb1
Sal Espinosa <sespinos@gmail.com>
2015-10-12 23:49:47 -0400

    Outline javascript function for floating boxes.

---
+++ projects/beta/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+  // Box constructor function
+    function box(max_height, max_width, distance_from_top, percent_movement, speed) {
+        
+
+
+
+        this.animate() = function(callback){
+
+        }
+    };
+
+
+var box1 = new box(280, 700, 90, 70%, 400);
+var box2 = new box(280, 700, 90, 70%, 400);
+var box3 = new box(280, 700, 90, 70%, 400);
+var box4 = new box(280, 700, 90, 70%, 400);
+
+var boxes = [box1, box2, box3, box4];
+
+// each
+
+}
+
