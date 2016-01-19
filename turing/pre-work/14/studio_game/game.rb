abffc1c4a000466141b95f772d576c870024b251
Sal Espinosa <sespinos@gmail.com>
2016-01-19 07:24:31 -0600

    Chapter 14 initial commit

--- turing/pre-work/14/studio_game/game.rb
+++ turing/pre-work/14/studio_game/game.rb
@@ -1,5 +1,6 @@
 require_relative 'player'
 require_relative 'die'
+require_relative 'take_turn'
 
 class Game
   def initialize(name)
@@ -19,17 +20,7 @@ class Game
       puts player
     end
     @players.each do |player|
-      player_die = Die.new
-
-      case player_die.roll
-      when 5, 6
-        player.w00t
-      when 3, 4
-        puts "#{player.name} was skipped"
-      else
-        player.blam
-      end
-
+      GameTurn.take_turn(player)
       puts player
     end
   end
