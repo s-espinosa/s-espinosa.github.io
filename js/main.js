fb423e5057342f0aabb7f744e498754c265a4b08
Sal Espinosa <sespinos@gmail.com>
2017-02-27 22:27:04 -0700

    Add JS transitions

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
 $( document ).ready(function() {
-  $("section.home").fadeIn(2000);
+  $("section.home").fadeIn(400);
 
-  $(".links").each(function(index, value) {
-    console.log(index)
-    console.log(value.innerHTML)
+  $(".links").on("click", function(){
+    deactivateLinks();
+    activateLink($(this));
+
+    var section = $(this).prop("classList")[1]
+    swapSection(section);
   });
 
+  function deactivateLinks() {
+    $(".links.active").removeClass("active")
+  }
+
+  function activateLink(button) {
+    button.addClass("active")
+  }
+
+  function swapSection(section) {
+    $("main section:visible").fadeOut(400, function() {
+      fadeInNewSection(section)
+    });
+  }
+
+  function fadeInNewSection(section) {
+    $("section." + section).fadeIn(400);
+  }
 });
